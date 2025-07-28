7787fc7c24d3a1d40847034425499c7bc4bf7ca5
Nguyen Duc Trung <nductrung779@gmail.com>
2025-07-28 10:54:25 +0700

    add file

---
+++ Finalize/db.py
@@ -0,0 +1,15 @@
+# db.py
+
+from pymongo import MongoClient
+from datetime import datetime
+
+client = MongoClient("mongodb+srv://nductrung779:12345@trr1.kfctgrg.mongodb.net/")
+db = client["chatbot"]
+chat_collection = db["history"]
+
+def save_chat(question, answer):
+    chat_collection.insert_one({
+        "question": question,
+        "answer": answer,
+        "timestamp": datetime.now()
+    })
